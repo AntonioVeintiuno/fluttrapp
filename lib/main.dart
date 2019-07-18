71db4561d77082e2ca53b5742b09d644892da51f
Antonio Garcia <moy_357@hotmail.com>
2019-07-17 20:06:47 -0500

    Adding init stateless widget.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
-void main() {
- var app =MaterialApp(
-   home: Scaffold(
-     appBar: AppBar(
-       title: Text("Firsty"),
-     ),
-     body: Container(
-       child: Center(
-         child: Text("Hello",
-         style: TextStyle(fontSize: 40),
-         ),
-       ),
-     ),
-     floatingActionButton: FloatingActionButton(
-       child: Icon(Icons.autorenew),
-       onPressed: () { print('click');},
-     ),
-   ),
- );
+void main() => runApp(MyApp());
 
-  runApp(app);
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext contex) {
+
+  }
+
 }
