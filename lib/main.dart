6c075a3b6273a50b93b49dfe4b1592c6b0f58690
Antonio Garcia <moy_357@hotmail.com>
2019-07-21 14:27:41 -0500

    Reorganize code.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/src/app.dart';
 
 void main() {
   runApp(MaterialApp(
     home: MyApp()
   ));
-}
-
-class MyApp extends StatelessWidget {
-  final double iconSize = 40.0;
-  final TextStyle textStyle = TextStyle(color: Colors.grey, fontSize: 30.0);
-
-  @override
-  Widget build(BuildContext contex) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Sateless widget"),
-      ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            MyCard(
-              title: Text('I Love Flutter',style: textStyle),
-              icon: Icon(Icons.favorite, color: Colors.red, size: iconSize),
-            ),
-            MyCard(
-              title: Text('I Like This Video',style: textStyle),
-              icon: Icon(Icons.thumb_up, color: Colors.blue, size: iconSize),
-            ),
-            MyCard(
-              title: Text('Next video',style: textStyle),
-              icon: Icon(Icons.queue_play_next, color: Colors.brown, size: iconSize),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-}
-
-class MyCard extends StatelessWidget {
-  final Widget title;
-  final Widget icon;
-
-  MyCard({this.title, this.icon});
-
-  @override
-  Widget build(BuildContext contex) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            this.title,
-            this.icon
-          ],
-        ),
-      )
-    );
-  }
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/src/screens/card.dart';
+
+class MyApp extends StatelessWidget {
+  final double iconSize = 40.0;
+  final TextStyle textStyle = TextStyle(color: Colors.grey, fontSize: 30.0);
+
+  @override
+  Widget build(BuildContext contex) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sateless widget"),
+        backgroundColor: Colors.red[300],
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MyCard(
+              title: Text('I Love Flutter',style: textStyle),
+              icon: Icon(Icons.favorite, color: Colors.red, size: iconSize),
+            ),
+            MyCard(
+              title: Text('I Like This Video',style: textStyle),
+              icon: Icon(Icons.thumb_up, color: Colors.blue, size: iconSize),
+            ),
+            MyCard(
+              title: Text('Next video',style: textStyle),
+              icon: Icon(Icons.queue_play_next, color: Colors.brown, size: iconSize),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
