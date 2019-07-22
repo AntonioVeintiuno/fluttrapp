b8b100d1af7fa84076d8d339010272c2bfa0a19f
Antonio Garcia <moy_357@hotmail.com>
2019-07-22 10:29:08 -0500

    Stateful widget first commit.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-
+import 'package:first_app/src/button.dart';
 void main() {
- var app =MaterialApp(
-   home: Scaffold(
-     appBar: AppBar(
-       title: Text("Firsty"),
-     ),
-     body: Container(
-       child: Center(
-         child: Text("Hello",
-         style: TextStyle(fontSize: 40),
-         ),
-       ),
-     ),
-     floatingActionButton: FloatingActionButton(
-       child: Icon(Icons.autorenew),
-       onPressed: () { print('click');},
-     ),
-   ),
- );
-
-  runApp(app);
+  runApp(
+    MaterialApp(home: MyButton())
+  );
 }
