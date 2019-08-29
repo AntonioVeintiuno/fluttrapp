fd06b2b8713f96b960bdf166db30edeb5f230d15
Antonio Garcia <moy_357@hotmail.com>
2019-08-29 08:58:29 -0500

    Adding tabs page.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:first_app/src/home_page.dart';
+// import 'package:first_app/src/home_page.dart';
+import 'package:first_app/src/tabs_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(home: HomePage())
+    // MaterialApp(home: HomePage())
+    MaterialApp(home: MyTabs())
   );
 }
